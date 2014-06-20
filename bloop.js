d07619c6fa8caa64e22a0994e6a34f770bc40c3e
John Martin <john@lonepixel.com>
2014-06-19 21:04:06 -0700

    Added an API function at /.

--- bloop.js
+++ bloop.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function(req, res){
+app.get('/', function(req, res) {
+    var routes = [];
+    for(var verb in app.routes) {
+        app.routes[verb].forEach(function(route) {
+            var doc = route.callbacks[route.callbacks.length-1].toString().split('\n').filter(function(str) {
+                var doc = str.match(/(?:\s\/\/)(.*)/);
+                if(doc == null) return false;
+                return true;
+            }).map(function(doc) {
+                return doc.match(/(?:\s\/\/)(.*)/)[1];
+            }).join('\n');
+            routes.push({method: verb, path: route.path, keys: route.keys, doc: doc});
+        });
+    }
+    res.json(routes);
+});
+
+app.get('/hello', function(req, res) {
     res.send('Hello World');
 });
 
-app.get('/json', function(req, res){
-    res.send({foo: 23, bar: "this is bar", baz: [1, 2, 3]});
+app.get('/json', function(req, res) {
+    res.json({foo: 23, bar: "this is bar", baz: [1, 2, 3]});
 });
 
 var server = app.listen(3000, function() {
