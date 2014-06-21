87c477759704a4760d3945d9936ca1c99163337b
John Martin <john@lonepixel.com>
2014-06-21 16:58:54 -0700

    Added login and more insertion order items.

--- bloop.js
+++ bloop.js
@@ -29,16 +29,38 @@ app.get('/json', function(req, res) {
     res.json({foo: 23, bar: "this is bar", baz: [1, 2, 3]});
 });
 
+//
+// Methods for a fake YDSP Mobile backend
+//
+
+app.get('/ydsp/login', function(req, res) {
+    // Simple login method.
+    console.log('Attemping YDSP login.');
+    if (req.query.username == 'jmartin') {
+        res.json({status: 'ok', message: 'Signed in user ' + req.query.username});
+    }
+    else {
+        res.json({status: 'error', message: 'Unknown username or password'});
+    }
+});
+
+var insertionOrders = [
+    {id: 0, name: "A Summer of Movies", advertiser: "Netflix", status: 'active', percentGoal: 0.82},
+    {id: 1, name: "Toyota EV Future March", advertiser: "Toyota", status: 'active', percentGoal: 0.73},
+    {id: 2, name: "Return to Microsoft Office 2014", advertiser: "Microsoft", status: 'active', percentGoal: 0.05},
+    {id: 3, name: "Frends & Family Plans Kevin Durant", advertiser: "Verizon", status: 'active', percentGoal: 0.21},
+    {id: 4, name: "Pantene Pro V Summer Promo", advertiser: "Proctor & Gamble", status: 'active', percentGoal: 0.67},
+    {id: 5, name: "Netflix Dusk to dawn - Norway", advertiser: "Netflix", status: 'active', percentGoal: 0.63},
+    {id: 6, name: "MS Xbox KRS", advertiser: "Microsoft", status: 'active', percentGoal: 0.69},
+    {id: 7, name: "MS Xbox One California Release", advertiser: "Microsoft", status: 'active', percentGoal: 0.99},
+    {id: 8, name: "Nike World Cup Phase 2 2014 - UK", advertiser: "Nike", status: 'active', percentGoal: 0.23},
+    {id: 9, name: "Nike World Cup Phase 2 2014 - Asia", advertiser: "Nike", status: 'active', percentGoal: 0.33}
+];
+
 app.get('/ydsp/insertion_orders', function(req, res) {
     // Returns YDSP-like insertion order records.
     console.log('Getting insertion orders.');
-    res.json({insertion_orders: [
-        {id: 0, name: "Hoefler & Co", advertiser: "Typography"},
-        {id: 1, name: "Toyota Fall Campaign", advertiser: "Toyota"},
-        {id: 2, name: "Microsoft Return to Office", advertiser: "Microsoft"},
-        {id: 3, name: "Framily Plans", advertiser: "AT&T"},
-        {id: 4, name: "Like a Tiger", advertiser: "Orange Mobile"}
-    ]});
+    res.json({status: 'ok', insertionOrders: insertionOrders});
 });
 
 var server = app.listen(3000, function() {
