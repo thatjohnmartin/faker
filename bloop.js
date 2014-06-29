3c33d6bacfcafffed9752b14a6a2389d27631daf
John Martin <john@lonepixel.com>
2014-06-28 17:51:57 -0700

    Upgraded to Express 4 and made the index HTML.

--- bloop.js
+++ bloop.js
@@ -1,32 +1,34 @@
 var express = require('express');
+var path = require('path');
+var swig = require('swig');
 var app = express();
 
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('/', function(req, res) {
-    // Returns a list of all active routes.
-    var routes = [];
-    for(var verb in app.routes) {
-        app.routes[verb].forEach(function(route) {
-            var doc = route.callbacks[route.callbacks.length-1].toString().split('\n').filter(function(str) {
-                var doc = str.match(/(?:\s\/\/)(.*)/);
-                if(doc == null) return false;
-                return true;
-            }).map(function(doc) {
-                return doc.match(/(?:\s\/\/)(.*)/)[1];
-            }).join('\n');
-            routes.push({method: verb, path: route.path, keys: route.keys, doc: doc});
-        });
+  // Returns a list of all active routes.
+  var routes = [];
+  for (var layer in app._router.stack) {
+    if (app._router.stack[layer].route) {
+      var route = app._router.stack[layer].route;
+      for (var method in route.methods) {
+        routes.push({method: method, path: route.path})
+      }
     }
-    res.json(routes);
+  }
+  res.render('index', {routes: routes});
 });
 
 app.get('/hello', function(req, res) {
-    // A little bit of text.
-    res.send('Hello, world.');
+  // A little bit of text.
+  res.send('Hello, world.');
 });
 
 app.get('/json', function(req, res) {
-    // A little bit of JSON.
-    res.json({foo: 23, bar: "this is bar", baz: [1, 2, 3]});
+  // A little bit of JSON.
+  res.json({foo: 23, bar: "this is bar", baz: [1, 2, 3]});
 });
 
 //
@@ -34,37 +36,37 @@ app.get('/json', function(req, res) {
 //
 
 app.get('/ydsp/login', function(req, res) {
-    // Simple login method.
-    console.log('Attemping YDSP login.');
-    if (req.query.username == 'jmartin') {
-        res.json({status: 'ok', message: 'Signed in user ' + req.query.username});
-    }
-    else {
-        res.json({status: 'error', message: 'Unknown username or password'});
-    }
+  // Simple login method.
+  console.log('Attemping YDSP login.');
+  if (req.query.username == 'jmartin') {
+    res.json({status: 'ok', message: 'Signed in user ' + req.query.username});
+  }
+  else {
+    res.json({status: 'error', message: 'Unknown username or password'});
+  }
 });
 
 var insertionOrders = [
-    {id: 0, name: "A Summer of Movies", advertiser: "Netflix", status: 'active', percentGoal: 0.82},
-    {id: 1, name: "Toyota EV Future March", advertiser: "Toyota", status: 'active', percentGoal: 0.73},
-    {id: 2, name: "Return to Microsoft Office 2014", advertiser: "Microsoft", status: 'active', percentGoal: 0.05},
-    {id: 3, name: "Frends & Family Plans Kevin Durant", advertiser: "Verizon", status: 'active', percentGoal: 0.21},
-    {id: 4, name: "Pantene Pro V Summer Promo", advertiser: "Proctor & Gamble", status: 'active', percentGoal: 0.67},
-    {id: 5, name: "Netflix Dusk to dawn - Norway", advertiser: "Netflix", status: 'active', percentGoal: 0.63},
-    {id: 6, name: "MS Xbox KRS", advertiser: "Microsoft", status: 'active', percentGoal: 0.69},
-    {id: 7, name: "MS Xbox One California Release", advertiser: "Microsoft", status: 'active', percentGoal: 0.99},
-    {id: 8, name: "Nike World Cup Phase 2 2014 - UK", advertiser: "Nike", status: 'active', percentGoal: 0.23},
-    {id: 9, name: "Nike World Cup Phase 2 2014 - Asia", advertiser: "Nike", status: 'active', percentGoal: 0.33},
-    {id: 9, name: "Nike World Cup Phase 2 2014 - US", advertiser: "Nike", status: 'active', percentGoal: 0.31}
+  {id: 0, name: "A Summer of Movies", advertiser: "Netflix", status: 'active', percentGoal: 0.82},
+  {id: 1, name: "Toyota EV Future March", advertiser: "Toyota", status: 'active', percentGoal: 0.73},
+  {id: 2, name: "Return to Microsoft Office 2014", advertiser: "Microsoft", status: 'active', percentGoal: 0.05},
+  {id: 3, name: "Frends & Family Plans Kevin Durant", advertiser: "Verizon", status: 'active', percentGoal: 0.21},
+  {id: 4, name: "Pantene Pro V Summer Promo", advertiser: "Proctor & Gamble", status: 'active', percentGoal: 0.67},
+  {id: 5, name: "Netflix Dusk to dawn - Norway", advertiser: "Netflix", status: 'active', percentGoal: 0.63},
+  {id: 6, name: "MS Xbox KRS", advertiser: "Microsoft", status: 'active', percentGoal: 0.69},
+  {id: 7, name: "MS Xbox One California Release", advertiser: "Microsoft", status: 'active', percentGoal: 0.99},
+  {id: 8, name: "Nike World Cup Phase 2 2014 - UK", advertiser: "Nike", status: 'active', percentGoal: 0.23},
+  {id: 9, name: "Nike World Cup Phase 2 2014 - Asia", advertiser: "Nike", status: 'active', percentGoal: 0.33},
+  {id: 9, name: "Nike World Cup Phase 2 2014 - US", advertiser: "Nike", status: 'active', percentGoal: 0.31}
 ];
 
 app.get('/ydsp/insertion_orders', function(req, res) {
-    // Returns YDSP-like insertion order records.
-    console.log('Getting insertion orders.');
-    res.json({status: 'ok', insertionOrders: insertionOrders});
+  // Returns YDSP-like insertion order records.
+  console.log('Getting insertion orders.');
+  res.json({status: 'ok', insertionOrders: insertionOrders});
 });
 
 var port = Number(process.env.PORT || 5000);
 var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
