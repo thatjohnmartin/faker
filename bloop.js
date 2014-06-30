49081bb74b3ca6b1d8c2675d091fbff4e1205d64
John Martin <john@lonepixel.com>
2014-06-29 21:28:47 -0700

    Added a thread handler.

--- bloop.js
+++ bloop.js
@@ -78,10 +78,46 @@ var threadpressCollections = [
   {id: 1004, handle: "threadpress.thedish", title: "The Dish"},
 ];
 
+var postBody1 = "Photo booth vinyl post-ironic stumptown. Vinyl lo-fi kogi ethical 8-bit +1. Cosby sweater put a bird on it Brooklyn \
+food truck shabby chic. Mustache banjo bitters, hoodie crucifix synth Godard post-ironic McSweeney's occupy Bushwick cardigan selvage \
+small batch. Forage Banksy readymade blog trust fund deep v. Disrupt XOXO DIY beard forage, meggings gluten-free hella Cosby sweater \
+literally Austin Pinterest paleo put a bird on it whatever. Tumblr banh mi occupy, food truck typewriter iPhone small batch bespoke trust \
+fund Carles Marfa sriracha literally.\n\
+\n\
+IPhone ethical occupy art party cred. Beard selfies PBR post-ironic chillwave food truck salvia disrupt. Letterpress artisan fap viral \
+Shoreditch, next level wayfarers iPhone Tonx quinoa. Letterpress you probably haven't heard of them Neutra Etsy. YOLO yr synth, squid \
+cliche gluten-free irony plaid American Apparel leggings cornhole pop-up. Pork belly Austin Schlitz, disrupt deep v ethnic pickled. Roof \
+party cray four loko, Pinterest swag umami selvage.\n"
+
+var postBody2 = "Seitan et bitters retro. Laborum mumblecore accusamus, disrupt cred whatever eiusmod blog fashion axe officia. \
+Adipisicing polaroid nulla salvia, street art **dreamcatcher** tempor. Seitan kitsch Cosby sweater authentic paleo nulla, Kickstarter banh mi \
+bespoke blog voluptate mustache dreamcatcher Banksy next level. Viral sriracha anim, do Cosby sweater forage kale chips authentic \
+try-hard cliche street art incididunt bicycle rights 90's. Brunch et flannel, aesthetic kogi drinking vinegar polaroid hashtag sustainable \
+YOLO tattooed. Chillwave mlkshk viral, four loko occaecat nulla YOLO ennui ullamco velit dolor Banksy wayfarers."
+
+var postBody3 = "Vice 8-bit try-hard, pug cardigan PBR ethical. PBR&B jean shorts irony vinyl, lo-fi umami Portland letterpress scenester \
+typewriter McSweeney's Godard tofu. Normcore XOXO cred pour-over Echo Park, High Life chia ethnic keffiyeh kogi tofu brunch. Tousled \
+distillery meh, pop-up tote bag fingerstache hoodie. Gluten-free Etsy yr, twee locavore photo booth raw denim deep v polaroid drinking \
+vinegar 90's chillwave cray literally leggings. Gastropub Banksy American Apparel pour-over blog chia. Small batch gastropub four loko yr \
+forage cray pork belly."
+
+var firstPagePosts = [
+  {id: 2001, title: 'Climbing Avalanch Gulch on Shasta', tags: ['climbing', 'alpine', 'california', 'threadpress.post_status:published'],
+    created: '2014-06-20', markup: postBody1},
+  {id: 2002, title: 'Swift, a new language from Apple', tags: ['tech', 'languages', 'swift', 'threadpress.post_status:published'],
+    created: '2014-06-19', markup: postBody2},
+  {id: 2003, title: 'Red Rocks, NV', tags: ['climbing', 'sport-climbing', 'nevada', 'threadpress.post_status:published'],
+    created: '2014-06-16', markup: postBody3},
+  {id: 2004, title: 'New 12a in Pinnacles', tags: ['climbing', 'sport-climbing', 'california', 'threadpress.post_status:published'],
+    created: '2014-06-11', markup: postBody1},
+  {id: 2005, title: 'Apple announces CloudKit', tags: ['tech', 'threadpress.post_status:published'],
+    created: '2014-06-03', markup: postBody2},
+];
+
 var collectionRoot = 'threadpress.';
 
 app.get('/collection', function(req, res) {
-  // Collection search.
+  // Search for a collection.
   var collections = [];
   if (req.query.handle == collectionRoot + '*') {
     collections = threadpressCollections;
@@ -90,9 +126,8 @@ app.get('/collection', function(req, res) {
 });
 
 app.get('/collection/handle/:handle', function(req, res) {
-  // Collection get.  
+  // Get a collection.  
   var collection = null;
-  console.log(req.params.handle.substr(0, collectionRoot.length));
   if (req.params.handle.substr(0, collectionRoot.length) == collectionRoot) {
     collection = _.find(threadpressCollections, function(c) { return c.handle == req.params.handle; });
   }
@@ -102,6 +137,12 @@ app.get('/collection/handle/:handle', function(req, res) {
   else {
     res.json({status: 'ok', collection: collection});
   }
+});
+
+app.get('/thread/collection/:handle', function(req, res) {
+  // Get a thread.
+  var thread = {'posts': firstPagePosts};
+  res.json({status: 'ok', thread: thread});
 });
 
 var port = Number(process.env.PORT || 5000);
